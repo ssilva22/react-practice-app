6e0d83d3b9a9c28b525a551825b7640275290478
Sebastian <sebastianwebp@gmail.com>
2021-09-01 09:56:21 -0400

    added sign-in and sign-up page as well as sign-in component. Needs styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./App.css"
 import HomePage from "./pages/homepage/homepage.component"
-import {Switch, Route } from "react-router-dom"
+import {Switch, Route} from "react-router-dom"
 import ShopPage from "./pages/shop/shop.component"
 import Header from "./components/header/header.component"
-
- 
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"
 
 function App() {
   return (
     <div className="App">
-    <Header/>
-    <Switch>
-    <Route exact path="/" component={HomePage }/>
-    <Route exact path="/shop" component={ShopPage}/>
-   </Switch>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUpPage} />
+      </Switch>
     </div>
   )
 }
