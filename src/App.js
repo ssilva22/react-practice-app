e112db74d33fd63cf155f97f5621fb63f6f6c21d
Sebastian <sebastianwebp@gmail.com>
2021-09-06 21:48:27 -0400

    added extra state to the class-based component in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css"
 import React from "react"
+import {auth} from "./firebase/firebase.utils"
 import HomePage from "./pages/homepage/homepage.component"
 import {Switch, Route} from "react-router-dom"
 import ShopPage from "./pages/shop/shop.component"
 import Header from "./components/header/header.component"
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"
-import {auth} from "./firebase/firebase.utils"
 
-class App extends React.Component() {
+class App extends React.Component {
   constructor() {
     super()
 
@@ -31,14 +31,16 @@ class App extends React.Component() {
   }
 
   render() {
-    ;<div className="App">
-      <Header currentUser={this.state.currentUser} />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/signin" component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
+    return (
+      <div className="App">
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    )
   }
 }
 
