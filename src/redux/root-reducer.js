fbdfade409ff4f308493007f2561a9edbf9f20de
Sebastian <sebastianwebp@gmail.com>
2021-09-08 19:08:07 -0400

    added redux-persist to maintain state

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,21 @@
 //This object represents all of the state of our project.
 import {combineReducers} from "redux"
+import {persistReducer} from "redux-persist"
+import storage from "redux-persist/lib/storage"
 
 import userReducer from "./user/user.reducer"
 
 import cartReducer from "./cart/cart.reducer"
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 })
+
+export default persistReducer(persistConfig, rootReducer)
