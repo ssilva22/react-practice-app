e32624ffdf6f2d9ee06b02a7a9c889cb2dc8fc41
Sebastian <sebastianwebp@gmail.com>
2021-09-09 13:40:17 -0400

    fixed collections bug and added new redux data flow for shop components

--- src/components/collection-preview/collection-preview.component.js
+++ src/components/collection-preview/collection-preview.component.js
@@ -2,14 +2,14 @@ import React from "react"
 
 import CollectionItem from "../collection-item/collection-item.component"
 
-import "./preview-collection.styles.scss"
+import "./collection-preview.styles.scss"
 
-const PreviewCollection = ({title, items}) => (
+const CollectionPreview = ({title, items}) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
       {items
-        .filter((item, index) => index < 4)
+        .filter((item, idx) => idx < 4)
         .map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
@@ -17,4 +17,4 @@ const PreviewCollection = ({title, items}) => (
   </div>
 )
 
-export default PreviewCollection
+export default CollectionPreview
--- src/components/collections-overview/collections-overview.component.js
+++ src/components/collections-overview/collections-overview.component.js
@@ -1,21 +1,23 @@
 import React from "react"
 import {connect} from "react-redux"
 import {createStructuredSelector} from "reselect"
-import {selectCollections} from "../../redux/shop/shop.selectors.js"
-import PreviewCollection from "../preview-collection/preview-collection.component.js"
 
-import "./collections-overview.component.js"
+import CollectionPreview from "../collection-preview/collection-preview.component"
+
+import {selectCollectionsForPreview} from "../../redux/shop/shop.selectors"
+
+import "./collections-overview.styles.scss"
 
 const CollectionsOverview = ({collections}) => (
   <div className="collections-overview">
     {collections.map(({id, ...otherCollectionProps}) => (
-      <PreviewCollection key={id} {...otherCollectionProps} />
+      <CollectionPreview key={id} {...otherCollectionProps} />
     ))}
   </div>
 )
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 })
 
 export default connect(mapStateToProps)(CollectionsOverview)
