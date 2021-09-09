cf0532db119712489b1ede547c7bdc8ca1d95e35
Sebastian <sebastianwebp@gmail.com>
2021-09-09 15:27:47 -0400

    added stripe integration for payments and fixed some styling issues

--- src/pages/checkout/checkout.component.js
+++ src/pages/checkout/checkout.component.js
@@ -5,6 +5,7 @@ import {createStructuredSelector} from "reselect"
 import {selectCartItems, selectCartTotal} from "../../redux/cart/cart.selectors"
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component"
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component"
 
 import "./checkout.styles.scss"
 
@@ -30,9 +31,8 @@ const CheckoutPage = ({cartItems, total}) => (
     {cartItems.map((cartItem) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
-    <div className="total">
-      <span>TOTAL: ${total}</span>
-    </div>
+    <div className="total">TOTAL: ${total}</div>
+    <StripeCheckoutButton price={total} />
   </div>
 )
 
